e643e92f5ec21a2a56fb9ca543bf1384b1c8f798
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2022-05-11 00:45:01 -0400

    Added a test that works

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders 'Preview without a YNAB account' link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Preview without a YNAB account/i, {
+    selector: "button",
+  });
   expect(linkElement).toBeInTheDocument();
 });
